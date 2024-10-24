61aa4ba07d8f7617b8882244a098aee5e3acc3c7
Andhika <jne.mahkota2@gmail.com>
2024-10-25 04:02:29 +0700

    perbaikan fiture

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@
 
 const burger = document.querySelector('#burger')
 const navMenu = document.querySelector('#nav-menu')
+const toTop = document.querySelector('#to-top')
 
 burger.addEventListener('click', function() {
     burger.classList.toggle('burger-active')
@@ -16,7 +17,20 @@ window.onscroll = function() {
 
     if(window.scrollY > fixedNav) {
         header.classList.add("navbar-fixed")
+        toTop.classList.remove("hidden")
+        toTop.classList.add("flex")
     } else {
         header.classList.remove("navbar-fixed")
+        toTop.classList.remove("flex")
+        toTop.classList.add("hidden")
     }
 }
+
+// Hide Burger Klik di luar Burger
+
+window.addEventListener('click', function(e) {
+    if(e.target != burger && e.target != navMenu) {
+        burger.classList.remove('burger-active')
+        navMenu.classList.add('hidden')
+    }
+})
